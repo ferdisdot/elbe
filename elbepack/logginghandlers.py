7ffe59fdaa72a12e8b2f6f4deac54f0789dccfdf
Ferdinand Schwenk <ferdinand.schwenk@emtrion.de>
2014-11-25 12:53:52 +0100

    Enhancements on the sqlite logging handler
    - Log more information
    - Some refactoring

--- elbepack/logginghandlers.py
+++ elbepack/logginghandlers.py
@@ -3,6 +3,38 @@ import os
 import sqlite3
 from datetime import datetime
 
+create_table_sql  ="""CREATE TABLE IF NOT EXISTS logging(
+    created datetime,
+    name text,
+    level integer,
+    levelname text,
+    module text,
+    pathname text,
+    lineno integer,
+    func text,
+    process int,
+    thread text,
+    threadName text,
+    Exception text, 
+    msg text
+)"""
+
+insert_sql = """INSERT INTO logging(
+    created,
+    name,
+    level,
+    levelname,
+    module,
+    pathname,
+    lineno,
+    func,
+    process,
+    thread,
+    threadName,
+    exception,
+    msg
+)
+VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)"""
 
 class SQLiteHandler(logging.Handler): # Inherit from logging.Handler
     """
@@ -13,22 +45,29 @@ class SQLiteHandler(logging.Handler): # Inherit from logging.Handler
         logging.Handler.__init__(self)
         # Our custom argument
         self.baseFilename = filename
-        self.db = sqlite3.connect(filename) # might need to use self.filename
-        self.db.execute("CREATE TABLE IF NOT EXISTS logging(created datetime, name text, level text, pathname text, lineno integer, func text, msg text)")
+        self.db = sqlite3.connect(self.baseFilename)
+        self.db.execute(create_table_sql)
         self.db.commit()
 
     def emit(self, record):
-        # record.message is the log message
-        thisdate = datetime.now()
-        self.db.execute(
-            'INSERT INTO logging(created, name, level, pathname, lineno, func, msg) VALUES(?,?,?,?,?,?,?)',
+        if record.exc_info:
+            exc_text = logging._defaultFormatter.formatException(record.exc_info)
+        else:
+            exc_text = None
+        self.db.execute(insert_sql,
             (
                 datetime.fromtimestamp(record.created),
                 record.name,
+                record.levelno,
                 record.levelname,
+                record.module,
                 os.path.abspath(record.filename),
                 record.lineno,
                 record.funcName,
+                record.process,
+                record.thread,
+                record.threadName,
+                exc_text,
                 record.getMessage(),
             )
         )
